9f04ff3a75fd495249e5f061c5b5b059b4c7d8ac
Park <neverzero9@gmail.com>
2017-11-12 11:37:19 +0700

    basic code kotlin

---
+++ src/kotlincode/SimpleCode/Collection/Predicates/To.kt
@@ -0,0 +1,19 @@
+package kotlincode.SimpleCode.Collection.Predicates
+
+/**
+ * ".toSet" , ".toMutableSet" ,
+ * ".toList" , ".toMutableList" ,
+ * ".toCollection" , ".toMutableCollection"
+ * Using to convert between collection type.
+ */
+
+fun main(args: Array<String>) {
+
+    var client: Collection<Customer> = setOf(Customer("Park", 50), Customer("Logan", 5900), Customer("Thor", 0))
+    var store: Set<Shop> = setOf(Shop("Magic Shop", client.toList()))
+
+    println(store)
+}
+
+private data class Shop(val name: String, val customers: List<Customer>)
+private data class Customer(var name: String, var money: Int)
